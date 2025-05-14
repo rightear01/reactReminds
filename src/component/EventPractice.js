e5f7f1ddddfb057fe00d44d72dba54cd9b8cc188
Cha jeong hawn <rightear01@gmail.com>
2025-05-14 09:57:17 +0900

    Synthetic Evnet 예제

--- src/component/EventPractice.js
+++ src/component/EventPractice.js
@@ -2,10 +2,12 @@ const EventPractice = () => {
     return (
         <div>
             <h1>이벤트 연습</h1>
-            <input type="text" name="message" placeholder="아무거나 입력해보세요." />
-            <button type="submit">확인</button>
-            <button type="reset">취소</button>
-            <div>입력한 내용: </div>
+            <input
+                type="text"
+                name="message"
+                placeholder="input something"
+                onChange={(e) => {console.log(e)}}
+            ></input>
         </div>
     );
 }
